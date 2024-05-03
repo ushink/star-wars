28d16a9b028ac41133eef47770ce22ca125cf4e9
Ulyana <ushinkevich@mail.ru>
2024-05-03 16:39:04 +0400

    rout + list star component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { StarListComponent } from './star-list/star-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/liststar', pathMatch: 'full' },
+  { path: 'liststar', component: StarListComponent, title: 'List Star' },
+];
---
+++ src/app/star-list/star-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-star-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './star-list.component.html',
+  styleUrl: './star-list.component.css'
+})
+export class StarListComponent {
+
+}
