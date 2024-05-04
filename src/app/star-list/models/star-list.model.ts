f4a92fa4bb82d5a9f1024a7ee8f512b4addc5b25
Ulyana <ushinkevich@mail.ru>
2024-05-04 19:05:06 +0400

    planet mock data + models ts

---
+++ src/app/star-list/models/star-list.model.ts
@@ -0,0 +1,24 @@
+export interface PlanetMock {
+  id: number;
+  name: string;
+  diameter: string;
+  climate: string;
+  terrain: string;
+}
+
+interface Planet {
+  name: string;
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: string;
+  population: string;
+  residents: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
