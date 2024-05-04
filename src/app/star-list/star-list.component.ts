f96c667b0af0428c9d1299bb3d2a1af9629baa60
Ulyana <ushinkevich@mail.ru>
2024-05-04 19:05:40 +0400

    planets page

--- src/app/star-list/star-list.component.ts
+++ src/app/star-list/star-list.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { PLANETS } from '../mock';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { PlanetMock } from './models/star-list.model';
 
 @Component({
   selector: 'app-star-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, UpperCasePipe],
   templateUrl: './star-list.component.html',
-  styleUrl: './star-list.component.css'
+  styleUrl: './star-list.component.css',
 })
 export class StarListComponent {
+  planets = PLANETS;
 
+  selectedPlanet?: PlanetMock;
+  onSelect(planet: PlanetMock, event: Event): void {
+    event.stopPropagation();
+    this.selectedPlanet = planet;
+    console.log('planet', planet.name);
+  }
 }
