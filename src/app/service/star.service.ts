19f4f86cec5b27b5510a04bfc49c2e4ac305ad2a
Ulyana <ushinkevich@mail.ru>
2024-05-10 17:45:43 +0400

    error handling

--- src/app/service/star.service.ts
+++ src/app/service/star.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Planet, PlanetsList } from '../models/star-list.model';
-import { Observable, forkJoin } from 'rxjs';
+import { Observable, catchError, forkJoin, of, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Residents } from '../models/star-detail.model';
 
@@ -12,17 +12,33 @@ export class StarService {
   private http = inject(HttpClient);
 
   getPlanets(page: number): Observable<PlanetsList> {
-    return this.http.get<PlanetsList>(`${this.apiUrl}planets/?page=${page}`);
+    return this.http
+      .get<PlanetsList>(`${this.apiUrl}planets/?page=${page}`)
+      .pipe(
+        catchError(this.handleError<PlanetsList>(`getPlanets page=${page}`))
+      );
   }
 
   getPlanet(id: number): Observable<Planet> {
-    return this.http.get<Planet>(`${this.apiUrl}planets/${id}`);
+    return this.http
+      .get<Planet>(`${this.apiUrl}planets/${id}`)
+      .pipe(catchError(this.handleError<Planet>(`getPlanet id=${id}`)));
   }
 
   getResidents(urls: string[] | null): Observable<Residents[]> {
     if (!urls) {
       return new Observable<Residents[]>();
     }
-    return forkJoin(urls.map((url) => this.http.get<Residents>(url)));
+    return forkJoin(urls.map((url) => this.http.get<Residents>(url))).pipe(
+      catchError(this.handleError<Residents[]>(`getResidents, [] `))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 }
